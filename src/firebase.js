3f778b1637e5f05c9855057b160adb9f78b2b35d
dlgnsrb227 <125418331+dlgnsrb227@users.noreply.github.com>
2023-07-25 18:19:25 +0900

    20230725

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAL4y9MlV0_goaMNwttUhdpYF99FpL7wd4",
+  authDomain: "hoongwitter.firebaseapp.com",
+  projectId: "hoongwitter",
+  storageBucket: "hoongwitter.appspot.com",
+  messagingSenderId: "288301861783",
+  appId: "1:288301861783:web:9e8cd350868622f4b9d2d2",
+};
+
+export default firebase.initializeApp(firebaseConfig);
